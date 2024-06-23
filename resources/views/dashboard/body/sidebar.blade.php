1701c9734729388bd1b7f7ea2310f48cd0d5f78a
Aldo Arista <aristaaldo2@gmail.com>
2024-06-23 16:58:14 +0700

    style: change website logos

--- resources/views/dashboard/body/sidebar.blade.php
+++ resources/views/dashboard/body/sidebar.blade.php
@@ -1,7 +1,8 @@
 <div class="iq-sidebar sidebar-default ">
     <div class="iq-sidebar-logo d-flex align-items-center justify-content-between">
         <a href="{{ route('dashboard') }}" class="header-logo">
-            <img src="{{ asset('assets/images/logo.png') }}" class="img-fluid rounded-normal light-logo" alt="logo">
+            <img src="{{ asset('assets/images/logo-kpm.png') }}" class="img-fluid rounded-normal light-logo"
+                alt="logo">
             <h5 class="logo-title light-logo ml-4" style="white-space: nowrap;">PT. KPM</h5>
         </a>
         <div class="iq-menu-bt-sidebar ml-0">
